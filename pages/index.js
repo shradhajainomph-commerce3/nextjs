39ef033e08783cd3c44e9cff8a3b47acf4424e8c
shradhajainomph-commerce3 <78423689+shradhajainomph-commerce3@users.noreply.github.com>
2021-03-01 11:06:52 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>A simple app to find the best prices: Howzzthat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
