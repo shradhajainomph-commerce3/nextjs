9f874b7ae4e36570159ad835f2e3d6c4d6b3d089
shradhajainomph-commerce3 <78423689+shradhajainomph-commerce3@users.noreply.github.com>
2021-03-01 12:04:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          The New Way To Search
+          Welcome to<a href="https://nextjs.org">Howzzthat</a>
         </h1>
 
         <p className={styles.description}>
